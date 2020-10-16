be5fe26f59188d3e5660b9e3f1261da38908969b
baolong0 <55168560+baolong0@users.noreply.github.com>
2020-10-16 11:10:44 +0700

    Update
    Update thuật toán hiển thị, quản lý user

---
+++ Web_11/Areas/Admin/Controllers/TintucsController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Web_11.Models;
+using Web_11.Models.data;
+
+namespace Web_11.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TintucsController : Controller
+    {
+        private readonly FootballNewsContext _context;
+
+        public TintucsController(FootballNewsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Tintucs
+        public async Task<IActionResult> Index()
+        {
+            TinTucModel tinTuc = new TinTucModel
+            {
+                Tintucs = _context.Tintuc.ToArray()
+            };
+            return View(tinTuc);
+        }
+        // GET: Admin/Tintucs/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tintuc = await _context.Tintuc
+                .FirstOrDefaultAsync(m => m.IdTinTuc == id);
+            if (tintuc == null)
+            {
+                return NotFound();
+            }
+
+            return View(tintuc);
+        }
+
+        // GET: Admin/Tintucs/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Tintucs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdTinTuc,TieuDe,Avatar,TomTat,LuotTuongTac,LuotXem,TrangThaiHienThi,TextNoiDung")] Tintuc tintuc)
+        {
+            if (ModelState.IsValid)
+            {
+                tintuc.LuotXem = 0;tintuc.LuotTuongTac = 0;
+                _context.Add(tintuc);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tintuc);
+        }
+
+        // GET: Admin/Tintucs/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tintuc = await _context.Tintuc.FindAsync(id);
+            if (tintuc == null)
+            {
+                return NotFound();
+            }
+            return View(tintuc);
+        }
+
+        // POST: Admin/Tintucs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("IdTinTuc,TieuDe,Avatar,TomTat,LuotTuongTac,LuotXem,TrangThaiHienThi,TextNoiDung")] Tintuc tintuc)
+        {
+            if (id != tintuc.IdTinTuc)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tintuc);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TintucExists(tintuc.IdTinTuc))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tintuc);
+        }
+
+        // GET: Admin/Tintucs/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tintuc = await _context.Tintuc
+                .FirstOrDefaultAsync(m => m.IdTinTuc == id);
+            if (tintuc == null)
+            {
+                return NotFound();
+            }
+
+            return View(tintuc);
+        }
+
+        // POST: Admin/Tintucs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var tintuc = await _context.Tintuc.FindAsync(id);
+            _context.Tintuc.Remove(tintuc);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TintucExists(string id)
+        {
+            return _context.Tintuc.Any(e => e.IdTinTuc == id);
+        }
+    }
+}
--- Web_11/Models/Data/Tintuc.cs
+++ Web_11/Models/Data/Tintuc.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Web_11.Models.data
 {
@@ -9,14 +10,24 @@ namespace Web_11.Models.data
         {
             SubTintuc = new HashSet<SubTintuc>();
         }
-
+        [Required(ErrorMessage = "ID không được bỏ trống")]
+        [Display(Name = "ID Tin Tức")]
         public string IdTinTuc { get; set; }
+        [DataType(DataType.Text)]
+        [Display(Name = "Tiêu đề tin tức")]
         public string TieuDe { get; set; }
+        [Required(ErrorMessage = "Phải tạo url")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "{0} dài {1} đến {2}")]
+        [Display(Name = "Url Avatar tin tức")]
         public string Avatar { get; set; }
+        [DataType(DataType.Text)]
+        [Display(Name = "Tóm Tắt Tin tức")]
         public string TomTat { get; set; }
         public int? LuotTuongTac { get; set; }
         public int? LuotXem { get; set; }
         public string TrangThaiHienThi { get; set; }
+        [DataType(DataType.Text)]
+        [Display(Name = "Nội dung tin")]
         public string TextNoiDung { get; set; }
 
         public virtual ICollection<SubTintuc> SubTintuc { get; set; }
